3fbb80c662f20fc58fcf4a05124ee6d7829c8a65
Alex Bulldog <alexey13031@gmail.com>
2022-10-25 18:45:11 +0300

    Removed add button out of index and added shared state

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,29 @@
 import type { NextPage } from "next";
-import { MouseEvent, useState } from "react";
+import { useState } from "react";
 
 import { Dropzone, Draggable } from "@/components/Draggable";
 import { TaskForm } from "@/components/TaskForm";
 import { trpc } from "@/utils/trpc";
 import { Task } from "@prisma/client";
 import Image from "next/future/image";
+import { AddButton } from "@/components/AddButton";
 
 const Home: NextPage = () => {
   const [dragged, setDragged] = useState<HTMLElement | null>(null);
+  const [isAddingCell, setIsAddingCell] = useState(false);
 
   const tasks = trpc.tasks.getAll.useQuery();
   const createTask = trpc.tasks.createTask.useMutation();
 
-  const handleAddTask = (e: MouseEvent, status: string) => {
-    const element = e.target as HTMLElement;
-    const attrs = element.getAttribute("class");
+  const handleAddTask = (status: string) => {
+    if (isAddingCell) return
 
     createTask.mutateAsync({ name: "New task", status: status }).then(() => {
       tasks.refetch();
-      element.setAttribute("class", attrs!);
+      setIsAddingCell(false);
     });
 
-    element.setAttribute("class", attrs + " animate-pulse");
+    setIsAddingCell(true);
   };
 
   const handleUpdateTask = (task: Task) => {
@@ -59,12 +60,10 @@ const Home: NextPage = () => {
                       </Draggable>
                     );
                   })}
-                <div
-                  onClick={(e) => handleAddTask(e, "created")}
-                  className="h-36 w-36 rounded-xl border-2 border-dashed border-slate-600 text-center text-7xl text-slate-600 hover:cursor-pointer hover:bg-slate-400"
-                >
-                  +
-                </div>
+                <AddButton
+                  disabled={isAddingCell}
+                  onClick={() => handleAddTask("created")}
+                />
               </Dropzone>
             </td>
 
@@ -88,12 +87,10 @@ const Home: NextPage = () => {
                       </Draggable>
                     );
                   })}
-                <div
-                  onClick={(e) => handleAddTask(e, "in-progress")}
-                  className="h-36 w-36 rounded-xl border-2 border-dashed border-slate-600 text-center text-7xl text-slate-600 hover:cursor-pointer hover:bg-slate-400"
-                >
-                  +
-                </div>
+                <AddButton
+                  disabled={isAddingCell}
+                  onClick={() => handleAddTask("in-progress")}
+                />
               </Dropzone>
             </td>
 
@@ -114,12 +111,10 @@ const Home: NextPage = () => {
                       </Draggable>
                     );
                   })}
-                <div
-                  onClick={(e) => handleAddTask(e, "finished")}
-                  className="h-36 w-36 rounded-xl border-2 border-dashed border-slate-600 text-center text-7xl text-slate-600 hover:cursor-pointer hover:bg-slate-400"
-                >
-                  +
-                </div>
+                <AddButton
+                  disabled={isAddingCell}
+                  onClick={() => handleAddTask("finished")}
+                />
               </Dropzone>
             </td>
           </tr>
@@ -127,7 +122,7 @@ const Home: NextPage = () => {
       </table>
       <a
         href="https://github.com/LeBulldoge/task-manager"
-        className="sticky bottom-0 flex gap-1 place-self-end pr-3 pb-1 text-xs text-slate-300 hover:underline group"
+        className="group sticky bottom-0 flex gap-1 place-self-end pr-3 pb-1 text-xs text-slate-300 hover:underline"
       >
         <Image
           src="/GitHub-Mark-Light-32px.png"
