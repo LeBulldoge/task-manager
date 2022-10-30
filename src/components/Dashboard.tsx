83fcd4666a02b9c63e0b9587a2d801fec4a1b5b1
Alex Bulldog <alexey13031@gmail.com>
2022-10-30 05:09:50 +0300

    Added Status reordering

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,25 +1,27 @@
 import { trpc } from "@/utils/trpc";
+import { Status } from "@prisma/client";
 import { ChangeEvent, useRef, useState } from "react";
 import { IconType } from "react-icons";
 import { MdHistory } from "react-icons/md";
 
 export const Dashboard = () => {
   const utils = trpc.useContext();
-  const statuses = trpc.tasks.getAllStatuses.useQuery({});
+  const statusQuery = trpc.tasks.getAllStatuses.useQuery({});
 
-  const invalidate = {
+  let shouldInvalidate = true;
+  const invalidateCallback = {
     async onSuccess() {
-      utils.tasks.invalidate();
+      if (shouldInvalidate) utils.tasks.invalidate();
     },
   };
-  const createStatus = trpc.tasks.createStatus.useMutation(invalidate);
-  const updateStatus = trpc.tasks.updateStatus.useMutation(invalidate);
-  const deleteStatus = trpc.tasks.deleteStatus.useMutation(invalidate);
+  const createStatus = trpc.tasks.createStatus.useMutation(invalidateCallback);
+  const updateStatus = trpc.tasks.updateStatus.useMutation(invalidateCallback);
+  const deleteStatus = trpc.tasks.deleteStatus.useMutation(invalidateCallback);
 
   const statusNameValidation = (e: ChangeEvent<HTMLInputElement>) => {
     const element = e.currentTarget;
     if (
-      statuses.data?.some(
+      statusQuery.data?.some(
         (s) => s.name == element.value && s.name != element.defaultValue
       )
     ) {
@@ -29,17 +31,46 @@ export const Dashboard = () => {
     }
   };
 
+  const handleSwapOrder = async (
+    e: ChangeEvent<HTMLInputElement>,
+    status: Status
+  ) => {
+    let value = -e.currentTarget.value;
+    const statusToSwap = statusQuery.data?.find((s) => {
+      return s != status && s.order === value;
+    });
+    if (statusToSwap) {
+      shouldInvalidate = false;
+      value = statusToSwap.order;
+    }
+    updateStatus
+      .mutateAsync({
+        where: { id: status.id },
+        data: { order: value },
+      })
+      .then(() => {
+        if (statusToSwap) {
+          shouldInvalidate = true;
+          updateStatus.mutate({
+            where: { id: statusToSwap.id },
+            data: { order: status.order },
+          });
+        }
+      });
+  };
+
   return (
     <div className="sticky top-0 left-0 bottom-0 z-50 flex min-h-screen basis-10 flex-col items-center gap-2 bg-slate-600 py-3 px-2 shadow">
       <DashGroup title="Status List" icon={MdHistory}>
-        <ul className="list-inside list-disc">
-          {statuses.data?.map((status) => {
+        <ul className="list-inside">
+          {statusQuery.data?.map((status) => {
             return (
               <li
                 key={status.id.toString()}
-                className="group/item mb-4 flex gap-2 rounded border border-slate-500 border-l-blue-300 p-2 text-blue-300 transition-all hover:-translate-x-1 hover:border-l-blue-200"
+                className="group/item mb-4 flex w-full gap-2 rounded border border-slate-500 border-l-blue-300 p-2 transition-all hover:-translate-x-1 hover:border-l-blue-200"
               >
                 <form
+                  className="flex w-full gap-2"
                   onSubmit={(e) => {
                     e.preventDefault();
                     const element =
@@ -51,19 +82,21 @@ export const Dashboard = () => {
                       data: { name: element.statusName.value },
                     });
                   }}
-                  onBlur={(e) => {
-                    const element =
-                      e.currentTarget as typeof e.currentTarget & {
-                        statusName: HTMLInputElement;
-                      };
-                    element.statusName.value = element.statusName.defaultValue;
-                  }}
+                  onBlur={(e) => e.currentTarget.reset()}
                 >
+                  <input
+                    name="statusOrder"
+                    type="number"
+                    defaultValue={-status.order}
+                    max={0}
+                    className="w-0 bg-inherit opacity-0 transition-all ease-linear hover:cursor-pointer group-hover/item:w-4 group-hover/item:opacity-100"
+                    onChange={(e) => handleSwapOrder(e, status)}
+                  />
                   <input
                     name="statusName"
                     type="text"
                     defaultValue={status.name}
-                    className="bg-inherit text-white transition-colors ease-linear invalid:rounded invalid:border invalid:border-rose-400"
+                    className="w-full flex-grow bg-inherit transition-colors ease-linear rounded border border-transparent invalid:border-rose-400"
                     onChange={statusNameValidation}
                   />
                   <input
@@ -81,7 +114,7 @@ export const Dashboard = () => {
           <div className="mb-4 border-b border-slate-500" />
           <li
             key="NEW"
-            className="mb-4 flex rounded border border-slate-500 border-l-blue-300 p-2 text-blue-300"
+            className="mb-4 flex rounded border border-slate-500 border-l-blue-300 p-2 text-blue-300 "
           >
             <form
               onSubmit={(e) => {
@@ -93,7 +126,7 @@ export const Dashboard = () => {
                   .mutateAsync({
                     data: {
                       name: element.statusName.value,
-                      order: statuses.data?.length ?? 0,
+                      order: statusQuery.data?.length ?? 0,
                     },
                   })
                   .then(() => (element.statusName.value = ""));
@@ -104,7 +137,7 @@ export const Dashboard = () => {
                   name="statusName"
                   type="text"
                   placeholder="Add a new status..."
-                  className="bg-inherit text-white transition-colors ease-linear invalid:rounded invalid:border invalid:border-rose-400"
+                  className="bg-inherit text-white transition-colors ease-linear rounded border border-transparent invalid:border-rose-400"
                   onChange={statusNameValidation}
                 />
               </label>
@@ -189,7 +222,7 @@ export const DashGroup = (props: {
       <div
         ref={drawer}
         hidden={true}
-        className="fixed top-0 left-0 bottom-0 right-12 flex -translate-x-full flex-col border-x border-slate-500 bg-slate-600 py-3 opacity-0 shadow transition-all duration-300 ease-in md:right-auto"
+        className="fixed top-0 left-0 bottom-0 right-12 flex -translate-x-full flex-col border-x border-slate-500 bg-slate-600 py-3 opacity-0 shadow transition-all duration-300 ease-in md:right-auto md:w-64"
       >
         <strong className="mb-5 w-full border-b border-slate-400 text-center text-2xl hover:cursor-default">
           {props.title}
